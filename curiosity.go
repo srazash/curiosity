b97b9d7c3050a01c6fc27e2c1e37b0c585d40297
Ryan Shaw-Harrison <srazash@protonmail.com>
2023-02-23 21:55:43 +0000

    Added some flavour text :)

--- curiosity.go
+++ curiosity.go
@@ -36,6 +36,11 @@ func main() {
 
 	client = &http.Client{Timeout: time.Second * 10}
 
+	fmt.Println("ｃｕｒｉｏｓｉｔｙ░ｖｅｒｓ．░０．９９　（くごフ）")
+	fmt.Println("【ｓｏｆｔｗａｒｅ　ｂｙ　ｒｓｈ】")
+	fmt.Println()
+	fmt.Printf("::download of curiosity sol %d files...\n", sol)
+
 	getPhotos(sol, url)
 
 }
@@ -64,7 +69,7 @@ func getPhotos(sol int, url string) {
 
 			filename := strings.Split(p.ImgSrc, "/")
 			file := fmt.Sprintf("%s/%s", dir, filename[len(filename)-1])
-			fmt.Printf("Downloading file %d of %d...\n", i+1, len(photos.Photos))
+			fmt.Printf("::::downloading file %d of %d...\n", i+1, len(photos.Photos))
 
 			out, err := os.Create(file)
 			if err != nil {
@@ -80,6 +85,7 @@ func getPhotos(sol int, url string) {
 		}
 
 	}
+
 }
 
 func getJson(url string, target interface{}) error {
